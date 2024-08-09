c5b7bd59fd2702f043d981fbb2032df32d8f4727
Henna Maria <hennamaria2001@gmail.com>
2024-08-09 20:20:15 +0530

    git conficts fixed

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import InteractionIcon from "./InteractionIcon";
 import LeftBar from "./layout/LeftBar";
+import NavBar from "./layout/NavBar";
 import RightBar from "./layout/RightBar";
 import UserIcon from "./UserIcon";
 import UserPreview from "./UserPreview";
@@ -8,6 +9,7 @@ import UserSetting from "./UserSetting";
 export {
     LeftBar,
     RightBar,
+    NavBar,
     UserIcon,
     InteractionIcon,
     UserPreview,

4eb779f53ca59c573c8129dcdc65f5fc99c991cf
ShijinA530 <shijinasasi835@gmail.com>
2024-08-12 00:49:34 +0530

    upgrade component added

--- src/pages/Story.jsx
+++ src/pages/Story.jsx
@@ -1,18 +1,16 @@
 import { ArrowLeft, CircleX, MessageSquare, Pencil } from "lucide-react";
 import { useState } from "react";
-import UserIcon from "../components/usericons/UserIcon"
+
+import UserIcon from "../components/usericons/UserIcon"; // Adjust the path according to your project structure
+import Upgrade from './../components/upgrademore/Upgrade'
 
 
 export default function Story() {
   const [clicked, setClicked] = useState(false);
 
-  const handleClose = () => {
-    setClicked(false);
-  };
-
   return (
     <div
-      className="relative flex h-screen bg-cover bg-center w-full md:w-1/3 mx-auto"
+      className="relative flex h-screen bg-cover bg-center w-full md:w-1/3 mx-auto items-center"
       style={{
         backgroundImage: `url('https://neural.love/cdn/ai-photostock/1eee2bb4-39ba-6ac4-8b66-ffe5a5909567/0.jpg?Expires=1727740799&Signature=MXr7E5bEM7G30qmuVC1fJGI8C0i9MRWQ55bZg4HY9AVkJnp9z7UNwNAcBtMc8Zga-FkfjnWKdxP3NbE776q0kY81fxHYdEmsA2x1LnLGV5-ocUK3d-LnhHNU11YcrbxLn3I2vQs~GoZhDlbtxRwvjn~DU88-6zD9hn2pltiyLoJBO13nBd8EWEBNI1rvnUII3K1qOURxPIWbjL2EcviT4D~fLLAyOSHVNNIzbfUNX8ZuKTL~D5chW3xLjYQoVCPDhDEGIssdvSgBXL9c9TTXgTNoHughQGF-HmAnW8KtQuI1rHHnMZh3ABvLudKFiFJ9g94DHO1gL4B2qYuMMh2giQ__&Key-Pair-Id=K2RFTOXRBNSROX')`,
       }}
@@ -46,21 +44,7 @@ export default function Story() {
 
       {/* Modal */}
       {clicked && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-20">
-          <div className="bg-white px-8 rounded-3xl shadow-lg text-center pt-12 pb-4">
-            <h2 className="font-montserrat text-2xl font-medium mb-4">
-              Upgrade to
-              <br />
-              View Profile
-            </h2>
-            <button
-              onClick={handleClose}
-              className="px-4 py-2 bg-deep-plum text-white rounded-4xl hover:shadow-lg w-full"
-            >
-              Upgrade
-            </button>
-          </div>
-        </div>
+        <Upgrade />
       )}
     </div>
   );

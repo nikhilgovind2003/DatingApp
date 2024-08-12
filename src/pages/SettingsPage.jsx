499ca9c7a2119652e771005dde65e6a9006839b4
Henna Maria <hennamaria2001@gmail.com>
2024-08-12 11:54:04 +0530

    setting page fixed

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import PageTitle from '../components/PageTitle/PageTitle'
-import {  SearchIcon } from "lucide-react";
-import UserSetting from '../Components/usericons/UserSetting';
+import { Search, SearchIcon } from "lucide-react";
 import BottomNavbar from '../components/BottomNavbar';
+import { UserPreview, UserSetting } from '../Components';
 
 function SettingsPage() {
     return (
-        <div>
-            <PageTitle icon={SearchIcon} pageTitle="Settings" />
-            
-            <UserSetting />
-            <BottomNavbar/>
+        // <div>
+        //     <PageTitle icon={SearchIcon} pageTitle="Settings" />
+
+        //     <UserSetting />
+        //     <BottomNavbar/>
+        // </div>
+        <div className="bg-deep-plum h-screen overflow-y-auto">
+            <PageTitle icon={Search} pageTitle={"Settings"} />
+            <div className="rounded-t-4xl bg-white pt-5 px-5 pb-24 md:pb-5 sm:border-2 ">
+                <UserPreview 
+                qr={true}
+                name={'Nico Robin'}
+                bio={'Never give up 💪'}/>
+                <UserSetting />
+            </div>
         </div>
     )
 }

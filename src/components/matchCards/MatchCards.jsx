ba24cc5a7e578a74ab9032416906db631059b197
nikhilgovind2003 <govindnikhil508@gmail.com>
2024-08-13 18:15:42 +0530

    test

--- src/components/matchCards/MatchCards.jsx
+++ src/components/matchCards/MatchCards.jsx
@@ -2,6 +2,7 @@ import MatchCardComponent from "./MatchCardComponent";
 
   console.log(numberOfMatches);
   
+  const Matchcards = ({isNew}) => {
   return (
     <>
       <div className=" grid gap-4 grid-cols-2 lg:grid-cols-3">

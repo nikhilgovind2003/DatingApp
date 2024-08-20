6eb1bc68489447463f352e0b215424c20552175b
Henna <85387380+Hennamaria07@users.noreply.github.com>
2024-08-20 13:52:28 +0530

    Update DatingInterest.jsx

--- src/pages/DatingInterest.jsx
+++ src/pages/DatingInterest.jsx
@@ -7,7 +7,7 @@ const DatingInterest = () => {
   const onHandleSubmit = async (e) => {
     let interest = e.target.value;
     await axios
-      .post("http://localhost:5000/api/v1/users/profile-details", { interest })
+      .post("http://localhost:5000/api/v1/users/set-interest", { interest }, { withCredentials: true })
       .then(() => {
         navigate("/home");
       })

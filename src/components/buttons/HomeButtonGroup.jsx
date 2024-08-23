131e6a378d64ed5b98b798fcd2cbc570cd04a3d0
Henna Maria <hennamaria2001@gmail.com>
2024-08-23 09:19:54 +0530

    home tab button styling bug fixed

--- src/components/buttons/HomeButtonGroup.jsx
+++ src/components/buttons/HomeButtonGroup.jsx
@@ -1,46 +1,52 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const ButtonGroup = () => {
   const style =
     "px-2 py-2 text-center sm:px-4 sm:py-2 w-full rounded-full font-medium text-sm sm:text-lg md:text-xs lg:text-lg transition-all";
 
-  const [path, setpath] = useState("");
-  
-  console.log(path);
+  const location = useLocation();
+  const [path, setPath] = useState("");
+
+  useEffect(() => {
+    setPath(location.pathname.substring(1)); // Remove leading slash
+  }, [location.pathname]);
 
   return (
     <ul className="w-full hidden lg:flex space-x-2 rounded-full bg-pink-100 p-1 justify-between gap-1 sm:gap-2 lg:gap-5 my-5">
       <li
-        onClick={() => setpath("location")}
         className={
           path === "location"
             ? `${style} bg-primary text-blue-500`
             : `${style} bg-none text-gray-600`
         }
       >
-        <Link to="/location">Location</Link>
+        <Link to="/location" onClick={() => setPath("location")}>
+          Location
+        </Link>
       </li>
 
       <li
-        onClick={() => setpath("designation")}
         className={
           path === "designation"
             ? `${style} bg-primary text-blue-500`
             : `${style} bg-none text-gray-600`
         }
       >
-        <Link to="/designation">Designation</Link>
+        <Link to="/designation" onClick={() => setPath("designation")}>
+          Designation
+        </Link>
       </li>
       <li
-        onClick={() => setpath("qualification")}
         className={
           path === "qualification"
             ? `${style} bg-primary text-blue-500`
             : `${style} bg-none text-gray-600`
         }
       >
-        <Link to="/qualification">Qualification</Link>
+        <Link to="/qualification" onClick={() => setPath("qualification")}>
+          Qualification
+        </Link>
       </li>
     </ul>
   );

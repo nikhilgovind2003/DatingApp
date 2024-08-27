76635acce78b631a16cca96076ab89b04e6fac19
Reeshnave <reeshnave@gmail.com>
2024-08-28 01:06:19 +0530

    geting profiles by designation and genderpreference

--- src/pages/DesignationPage.jsx
+++ src/pages/DesignationPage.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ButtonGroup, InteractionIcon, MatchCardComponent, SubHeader, UserIcon } from "../Components";
 import { Userdata } from "../datas/Userdata";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const DesignationPage = () => {
+  const[users,setUsers] = useState([]);
+
+  const count= users.length
+  const featchDesignation = async()=>{
+    try {
+      const response= await axios.get('http://localhost:5000/api/v1/users/profile/designations',{ withCredentials: true })
+    console.log(response.data);
+    setUsers(response.data)
+    } catch (error) {
+      console.log(error);  
+    }  
+  }
+  useEffect(()=>{
+    featchDesignation();
+  },[])
+  console.log(users)
   return (
     <section className="sm: w-screen md:w-full lg:w-full pt-5 px-5 pb-24 md:pb-5 h-screen overflow-y-auto overflow-x-hidden">
       <div>
@@ -24,19 +41,19 @@ const DesignationPage = () => {
         <SubHeader title="Designation" />
         <InteractionIcon />
         <p className="text-text font-medium my-3 text-lg">
-          Your Matches <span className="text-light-purple">42</span>
+          Your Matches <span className="text-light-purple">{count}</span>
         </p>
       </div>
       <div className="grid xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-3 grid-cols-2 gap-5">
-        {Userdata?.map((user, i) => (
-           <Link to={`/profile/${user.id}`} 
+        {users?.map((user, i) => (
+           <Link to={`/profile/${user._id}`} 
            key={i} >
           <MatchCardComponent
             key={i}
             isNew={false}
-            img={user.img}
+            img={user.profileImage.url}
             distance={user.distance}
-            name={user.firstName}
+            name={user.name}
             age={user.age}
             place={user.place}
             match={user.match}

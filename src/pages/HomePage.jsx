bd5fa6a9a52260f45893684b0a32a364a4bda38d
mohammed shajeeh <shajeehppmltr71@gmail.com>
2024-08-28 12:50:26 +0530

    add userdata home page and profile page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Userdata } from "../datas/Userdata";
 import { ButtonGroup, Sidemenu, UserIcon } from "../Components";
 import { HiOutlineBell } from "react-icons/hi";
@@ -8,8 +8,24 @@ import Rightside from "../components/rightsidemenu/Rightside";
 import axios from "axios";
 
 const HomePage = () => {
- 
- 
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:5000/api/v1/users/users"
+      ); // Fetch all users from your backend
+      setUsers(response.data); // Store fetched users in state
+      console.log("Fetched Data:", response.data);
+    } catch (error) {
+      console.log("Error fetching users:", error);
+    }
+  };
+
   return (
     <section className="lg:w-full md:w-full sm: w-screen pt-5 px-5 pb-24 md:pb-5 h-screen overflow-y-auto overflow-x-hidden">
       <div className="flex justify-between md:hidden gap-5">
@@ -23,7 +39,6 @@ const HomePage = () => {
           </button>
           <div className="pt-3">
             <Rightside />
-
           </div>
         </div>
       </div>
@@ -32,30 +47,32 @@ const HomePage = () => {
           <UserIcon add={"purple"} />
           <p className="mt-0.5 text-[14px]">My Story</p>
         </button>
-        {Userdata?.map((user, i) => (
+        {users?.map((user, i) => (
           <button key={i}>
-            <UserIcon key={user.id} story={true} url={user.img} />
-            <p className="mt-0.5 text-[14px]">{user.firstName}</p>
+            <UserIcon key={user.id} story={true} url={user.profileImage.url} />
+            <p className="mt-0.5 text-[14px]">{user.user?.firstName}</p>
           </button>
         ))}
       </div>
       <ButtonGroup />
       <div className="grid xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-3 grid-cols-2 gap-5">
-        {Userdata.map((user, i) => {
+        {users.map((user, i) => {
           const name = user.firstName + " " + user.lastName;
           return (
-
-           <Link to={`/profile/${user.id}`}>
-            <HomeCardComponents       
-              key={i}
-              img={user.img}
-              name={user.name}
-              sex={user.gender}
-              age={user.age}
-              job={user.job}
-              place={user.place}
-              
-            />
+            // <Link to={`/profile/${user.id}`}>
+            <Link to={`/profile/${user.user._id}`}>
+            {/* <Link to={`/profile`}> */}
+              <HomeCardComponents
+                key={i}
+                img={user.profileImage.url} // Correctly accessing the image URL
+                name={`${user.user?.firstName} ${user.user?.lastName}`}
+                sex={user.gender}
+                job={user.qualification}
+                age={user.age}
+                place={user.place}
+                isActive={user.user?.isActive}
+                
+              />
             </Link>
           );
         })}

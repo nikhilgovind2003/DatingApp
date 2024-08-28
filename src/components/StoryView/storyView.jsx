6e64250cc89217ba94d9228170aa74b4d084d827
nikhilgovind2003 <govindnikhil508@gmail.com>
2024-08-28 10:40:07 +0530

    Add video into story part

--- src/components/StoryView/storyView.jsx
+++ src/components/StoryView/storyView.jsx
@@ -3,47 +3,54 @@ import { Userdata } from "../../datas/Userdata";
 import { UserIcon } from "../index";
 import { config } from "dotenv";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const storyView = () => {
+  const [stories, setStories] = useState([]);
+
+  useEffect(() => {
+    const getStories = async () => {
+      try {
+        const res = await axios.get("http://localhost:5000/story", config);
+        let userData = res.data.userStories;
+        setStories(
+          userData.map((user) => ({
+            id: user._id,
+            profileImage: user.profileImage.url,
+          }))
+        );
+        console.log("Successfully fetched stories");
+      } catch (error) {
+        console.error("Error fetching stories:", error);
+      }
+    };
+
+    getStories();
+  }, []);
+
+
+  stories.map(U => {
+    console.log(U.profileImage);
+    
+  })
 
 
 
-    const [getStory, setgetStory] = useState({})
-
-
-    useEffect(() => {
-        try {
-            const getStories = async() => {
-                
-                const res = await axios.get("http://localhost:5000/getStories", config)
-                setgetStory(res.data.userStories)
-                console.log("Successfully");
-            }
-            
-            getStories()
-
-        } catch (error) {
-            console.log(error);
-            
-        }
-    },[])
 
 
 
-
-
-
-
-    return (
-        <div className=" flex gap-4">
+  return (
+    <div className=" flex gap-4">
       <button>
         <UserIcon add={"purple"} />
         <p className="mt-0.5 text-[14px]">My Story</p>
       </button>
-      {Userdata?.map((user, i) => (
+      {stories?.map((user, i) => (
         <button key={i}>
-          <UserIcon key={user.id} story={true} url={user.img} />
-          <p className="mt-0.5 text-[14px]">{user.firstName}</p>
+          <Link to={`/story/${user.id}`}>
+            <UserIcon key={user.id} story={true} url={user.profileImage} />
+            <p className="mt-0.5 text-[14px]">Arjun</p>
+          </Link>
         </button>
       ))}
     </div>

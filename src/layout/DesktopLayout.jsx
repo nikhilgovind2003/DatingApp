1f5a6dede35d812628e5c55d705e97467ec158cc
ShijinA530 <shijinasasi835@gmail.com>
2024-09-01 21:27:38 +0530

    name dynamically added

--- src/layout/DesktopLayout.jsx
+++ src/layout/DesktopLayout.jsx
@@ -23,22 +23,22 @@ const DesktopLayout = ()=>{
         transition={Bounce}
     />
         <div className="grid md:grid-cols-12 gap-6 min-h-screen overflow-hidden w-screen">    
-        <aside className="md:col-span-3 md:grid hidden overflow-hidden  h-screen">
-            <LeftBar />
-        </aside>
-    
+            <aside className="md:col-span-3 md:grid hidden overflow-hidden  h-screen">
+                <LeftBar />
+            </aside>
         
-        <main className="md:col-span-6 ">
-            <Outlet />
-            <BottomNavbar 
-            className="md:hidden mx-2"/>
-        </main>
-    
-       
-        <aside className="md:col-span-3 md:grid hidden overflow-x-auto">
-            <RightBar />
-        </aside>
-    </div>
+            
+            <main className="md:col-span-6 ">
+                <Outlet />
+                <BottomNavbar 
+                className="md:hidden mx-2"/>
+            </main>
+        
+        
+            <aside className="md:col-span-3 md:grid hidden overflow-x-auto">
+                <RightBar />
+            </aside>
+        </div>
     </>
   );
 };

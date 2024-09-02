0d4ff7eb94542c143fec871ac17fb5c208a69aa2
Henna Maria <hennamaria2001@gmail.com>
2024-09-02 19:35:31 +0530

    unwanted field removed

--- src/redux/features/auth/authSlice.js
+++ src/redux/features/auth/authSlice.js
@@ -5,7 +5,6 @@ const initialState = {
     isAuthenticated: sessionStorage.getItem('isAuthenticated') === 'true',
     userInfo: sessionStorage.getItem('userInfo') ? JSON.parse(sessionStorage.getItem('userInfo')) : null,
     token: sessionStorage.getItem('token'),
-    tokenExpiry: sessionStorage.getItem('tokenExpiry')
 };
 
 const authSlice = createSlice({
@@ -13,36 +12,30 @@ const authSlice = createSlice({
     initialState,
     reducers: {
         signup: (state, action) => {
-            const { isAuthenticated, userInfo, token, tokenExpiry } = action.payload;
+            const { isAuthenticated, userInfo, token } = action.payload;
             sessionStorage.setItem('isAuthenticated', String(isAuthenticated));
             sessionStorage.setItem('userInfo', JSON.stringify(userInfo));
             sessionStorage.setItem('token', token);
-            sessionStorage.setItem('tokenExpiry', tokenExpiry);
             state.isAuthenticated = isAuthenticated;
             state.userInfo = userInfo;
             state.token = token;
-            state.tokenExpiry = tokenExpiry;
         },
         login: (state, action) => {
-            const { isAuthenticated, userInfo, token, tokenExpiry } = action.payload;
+            const { isAuthenticated, userInfo, token } = action.payload;
             sessionStorage.setItem('isAuthenticated', String(isAuthenticated));
             sessionStorage.setItem('userInfo', JSON.stringify(userInfo));
             sessionStorage.setItem('token', token);
-            sessionStorage.setItem('tokenExpiry', tokenExpiry);
             state.isAuthenticated = isAuthenticated;
             state.userInfo = userInfo;
             state.token = token;
-            state.tokenExpiry = tokenExpiry;
         },
         logout: (state) => {
             sessionStorage.removeItem('isAuthenticated');
             sessionStorage.removeItem('userInfo');
             sessionStorage.removeItem('token');
-            sessionStorage.removeItem('tokenExpiry');
             state.isAuthenticated = false;
             state.userInfo = null;
             state.token = null;
-            state.tokenExpiry = null;
         }
     }
 });

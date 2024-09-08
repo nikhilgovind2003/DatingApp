eb60f5eaf4ed770d2a7c44f640673a272049f99f
Nikhil <84319053+nikhilgovind2003@users.noreply.github.com>
2024-09-08 16:36:11 +0530

    Revert "Chat app"

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,14 @@ import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
-import { SocketProvider } from "./context/SocketContext.jsx";
+import React from "react";
+
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <ChakraProvider>
-      {/* <React.StrictMode> */}
-      <SocketProvider>
+      <React.StrictMode>
         <App />
-      </SocketProvider>
-      {/* </React.StrictMode> */}
+      </React.StrictMode>
     </ChakraProvider>
   </Provider>
 );
